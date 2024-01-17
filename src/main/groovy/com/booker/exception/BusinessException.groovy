a1b082f066cae3a86ff1f20f4dc403590f5643b3
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2024-01-17 06:47:05 -0300

    Criando BusinessException

---
+++ src/main/groovy/com/booker/exception/BusinessException.groovy
@@ -0,0 +1,12 @@
+package com.booker.exception
+
+class BusinessException extends RuntimeException {
+
+    public BusinessException(String message){
+        super(message)
+    }
+
+    public BusinessException(String message, Throwable cause){
+        super(message, cause)
+    }
+}
